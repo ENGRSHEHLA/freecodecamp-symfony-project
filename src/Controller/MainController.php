574cf2329cbcb483d7e452412698973c63ffa589
Shehla <sahmed@coupons4u.de>
2026-04-13 12:34:39 +0200

    Initial Symfony project updates

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends AbstractController
+{
+    #[Route("/", name: "app_main")]
+    public function index()
+    {
+        return $this->render(view: 'home/index.html.twig');
+    }
+    #[Route("/custome/{name}", name: "custome")]
+    public function custome(Request $request)
+    {
+        $name = $request->get(key: 'name');
+        return $this->render('home/custome.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
