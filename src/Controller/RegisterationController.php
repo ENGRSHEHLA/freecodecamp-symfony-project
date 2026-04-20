29202bf3ba61ed65f4bb38e3a05ded7d75ee2419
Shehla <sahmed@coupons4u.de>
2026-04-20 13:06:51 +0200

    implement user signup, login, and custom authenticator.

---
+++ src/Controller/RegisterationController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RegisterationController extends AbstractController
+{
+    #[Route('/registeration', name: 'app_registeration')]
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'invalid_message' => 'The password fields must match.',
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+            ])
+            ->add('register', SubmitType::class, [
+                'label' => 'Register',
+                'attr' => ['class' => 'btn btn-success float-right'],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $plainPassword)
+            );
+
+            $em->persist($user);
+            $em->flush();
+
+            return new Response('Registration successful');
+        }
+
+        return $this->render('registeration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
